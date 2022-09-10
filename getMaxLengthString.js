cb06e52b09c883eb3efcb18ef13d342f41d0050a
darKKOREO <tanapat.daoloy@mail.kmutt.ac.th>
2022-09-11 00:00:10 +0700

    1st_commit

--- getMaxLengthString.js
+++ getMaxLengthString.js
@@ -1,6 +1,13 @@
 const { template } = require('@babel/core')
 
 function getMaxLengthString(arrayOfString) {
-  //code here
+  if(arrayOfString === null || arrayOfString=== undefined){
+    return undefined
+  }else{
+     let arr = arrayOfString.map(len =>len.length)
+     let lenMax = Math.max(...arr)
+     return arrayOfString.filter(len =>len.length ===lenMax)
+  }
+
 }
 module.exports = getMaxLengthString
